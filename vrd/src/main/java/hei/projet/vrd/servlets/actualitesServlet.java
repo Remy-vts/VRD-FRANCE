8f448f1627f4a026e0c09802cc4714ce0b43ee13
remy <remy.vitse@hei.fr>
2017-02-11 18:23:33 +0100

    Ajout responsive design tablette

--- vrd/src/main/java/hei/projet/vrd/servlets/actualitesServlet.java
+++ vrd/src/main/java/hei/projet/vrd/servlets/actualitesServlet.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 
 
