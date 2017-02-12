6191b3481f5c03fc840bfd37af1aeebb69803d24
remy <remy.vitse@hei.fr>
2017-02-12 18:12:12 +0100

    Modification DataSourceProvider

--- vrd/src/main/java/hei/projet/vrd/dao/impl/DataSourceProvider.java
+++ vrd/src/main/java/hei/projet/vrd/dao/impl/DataSourceProvider.java
@@ -1,22 +1,47 @@
 package hei.projet.vrd.dao.impl;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DataSourceProvider {
+	
+	private static class DataSourceProviderHolder {
+		private final static DataSourceProvider instance = new DataSourceProvider();
+	}
+	
+	public static DataSourceProvider getInstance() {
+		return DataSourceProviderHolder.instance;
+	}
 
-	private static MysqlDataSource dataSource;
+	private MysqlDataSource dataSource;
 
-	public static DataSource getDataSource() {
-		if (dataSource == null) {
-			dataSource = new MysqlDataSource();
-			dataSource.setServerName("localhost");
-			dataSource.setPort(3306);
-			dataSource.setDatabaseName("devweb_projet");
-			dataSource.setUser("root");
-			dataSource.setPassword("0000");
+	private DataSourceProvider() {
+		initDataSource();
+	}
+
+	private void initDataSource() {
+		Properties jdbcProperties = new Properties();
+		InputStream configFileStream = getClass().getClassLoader().getResourceAsStream("jdbc.properties");
+		try {
+			jdbcProperties.load(configFileStream);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		
+		dataSource = new MysqlDataSource();
+		dataSource.setServerName(jdbcProperties.getProperty("servername"));
+		dataSource.setPort(Integer.parseInt(jdbcProperties.getProperty("port")));
+		dataSource.setDatabaseName(jdbcProperties.getProperty("databasename"));
+		dataSource.setUser(jdbcProperties.getProperty("user"));
+		dataSource.setPassword(jdbcProperties.getProperty("password"));
+	}
+
+	public DataSource getDataSource() {
 		return dataSource;
 	}
 }
