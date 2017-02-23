f0f225832b7f3ee7ca2869c952dd539c7e302178
remy <remy.vitse@hei.fr>
2017-02-23 12:27:03 +0100

    Modification photo de présentation et début de la mise en place du controle de saisie

--- vrd/src/main/webapp/js/javascript.js
+++ vrd/src/main/webapp/js/javascript.js
@@ -15,3 +15,30 @@ function ConfirmOffre(idOffre) {
         window.location("deleteOffre?o="+idOffre);
     }
 }
+
+
+//AJOUT TITRE
+
+function inf_titre1() 
+{
+	document.getElementById("info_titre").style.display = "inline";
+}
+
+function inf_titre2() 
+{
+	document.getElementById("info_titre").style.display = "none";
+	var value = document.getElementById("info_titre").value.length;
+	result = value;
+	if(result>4 && result<51)
+	{
+		document.getElementById("check_titre1").style.display = "inline";
+		document.getElementById("check_titre2").style.display = "none";
+		return true;
+	}
+	else
+	{
+		document.getElementById("check_titre1").style.display = "none";
+		document.getElementById("check_titre2").style.display = "inline";
+		return false;
+	}
+}
