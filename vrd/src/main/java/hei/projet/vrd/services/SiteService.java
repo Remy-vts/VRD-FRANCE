49fd68e062084389f3ff37f99bc952f2ae4897a7
remy <remy.vitse@hei.fr>
2017-04-08 15:58:41 +0200

    Mise à niveau GIT

--- vrd/src/main/java/hei/projet/vrd/services/SiteService.java
+++ vrd/src/main/java/hei/projet/vrd/services/SiteService.java
@@ -12,6 +12,7 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 
 import hei.projet.vrd.dao.impl.ChantierDaoImpl;
+import hei.projet.vrd.dao.impl.ChiffresDaoImpl;
 import hei.projet.vrd.dao.impl.CoordonneesDaoImpl;
 import hei.projet.vrd.dao.impl.DirigeantDaoImpl;
 import hei.projet.vrd.dao.impl.EngagementsDaoImpl;
@@ -20,6 +21,7 @@ import hei.projet.vrd.dao.impl.MetierDaoImpl;
 import hei.projet.vrd.dao.impl.OffreDaoImpl;
 import hei.projet.vrd.dao.impl.PresseDaoImpl;
 import hei.projet.vrd.entities.Chantier;
+import hei.projet.vrd.entities.Chiffres;
 import hei.projet.vrd.entities.Coordonnees;
 import hei.projet.vrd.entities.Dirigeant;
 import hei.projet.vrd.entities.Engagements;
@@ -50,8 +52,7 @@ public class SiteService {
 	private MetierDaoImpl metierDao = new MetierDaoImpl();
 	private OffreDaoImpl offreDao = new OffreDaoImpl();
 	private CoordonneesDaoImpl coordonneesDao = new CoordonneesDaoImpl();
-	
-	
+	private ChiffresDaoImpl chiffreDao = new ChiffresDaoImpl();
 	
 	public List<Dirigeant> listDirigeant(){
 		return dirigeantDao.listDirigeant();
@@ -117,8 +118,6 @@ public class SiteService {
 								
 		return chantierDao.addChantier(chantier, "https://s3.eu-west-2.amazonaws.com/vrdfrance/"+picture.getSubmittedFileName());
 	}
-	
-	
 
 	public void deleteChantier(Integer id){
 		chantierDao.deleteChantier(id);
@@ -192,6 +191,15 @@ public class SiteService {
 		coordonneesDao.updateCoordonnees(mail, telephone);
 	}
 	
+	public List<Chiffres> listChiffres(){
+		return chiffreDao.listChiffres();
+	}
 	
+	public Chiffres getChiffres(Integer id){
+		return chiffreDao.getChiffres(id);
+	}
 	
+	public void updateChiffres(Integer id,  String titre, Integer chiffre){
+		chiffreDao.updateChiffres(id, titre, chiffre);
+	}
 }
